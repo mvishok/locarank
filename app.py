c0dce02e40923f707a39a97590a9bbadec7b5056
mvishok <vishokmanikantan@gmail.com>
2024-09-01 22:21:22 +0530

    feat: Add CSS styling for Swagger UI and index page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import factors.const as const
 from factors import education, finance, health, entertainment
 from flask import Flask, request
 
+from flasgger import Swagger
+
 colorama.init(autoreset=True)
 
 def calculate_total_score(scores):
@@ -16,9 +18,66 @@ def calculate_total_score(scores):
     return total_score
 
 app = Flask(__name__)
+app.config['url_sort_key'] = None
+app.config['SWAGGER'] = {
+    'title': 'LocaRank API',
+    'uiversion': 3,
+    'hide_top_bar': True,
+    'specs_route': '/',
+    'static_url_path': '/static',
+    'static_folder': 'static',
+}
+
+swagger = Swagger(app)
 
 @app.route('/score')
 def score():
+    """
+    Endpoint to get total score for a given latitude and longitude
+    ---
+    summary: Get total score for a location
+    description: This endpoint calculates the total score for a given location based on the latitude and longitude provided.
+    consumes:
+      - application/json
+    parameters:
+      - name: latitude
+        in: query
+        type: number
+        required: true
+        description: The latitude of the location.
+      - name: longitude
+        in: query
+        type: number
+        required: true
+        description: The longitude of the location.
+    responses:
+      200:
+        description: The total score and category-wise scores.
+        schema:
+          id: score
+          properties:
+            total_score:
+              type: number
+              description: The total score for the location.
+            category:
+              type: object
+              description: The category-wise scores.
+              properties:
+                Education:
+                  type: number
+                  description: The education score.
+                Healthcare:
+                  type: number
+                  description: The healthcare score.
+                Finance:
+                  type: number
+                  description: The finance score.
+                Entertainment:
+                  type: number
+                  description: The entertainment score.
+      400:
+        description: Error message.
+    """
 
     if 'latitude' not in request.args or 'longitude' not in request.args:
         return 'Error: Please provide both latitude and longitude.', 400
@@ -42,8 +101,6 @@ def score():
     }
 
     return result
-
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
