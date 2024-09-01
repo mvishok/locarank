ffbc965a8e7059557c128bc4102fcafab2548d5e
mvishok <vishokmanikantan@gmail.com>
2024-09-01 14:26:28 +0530

    chore: Add constants and weights for factors module

--- factors/const.py
+++ factors/const.py
@@ -1,1 +1,45 @@
 RADIUS=5000
+
+WEIGHTS = {
+    # Education
+    'school': 1.0,
+    'university': 2.0,
+    'library': 0.5,
+
+    # Entertainment
+    'park': 2,
+    'theatre': 4,
+    'museum': 3,
+    'cinema': 4,
+    'art_gallery': 3,
+    'zoo': 5,
+    'playground': 2,
+    'sports_centre': 4,
+    'swimming_pool': 3,
+
+    # Finance
+    'bank': 10,
+    'atm': 5,
+
+    # Health
+    'hospital': 5,
+    'pharmacy': 3,
+    'dentist': 2
+}
+
+CATEGORY = {
+    'Education': 0.10,
+    'Healthcare': 0.15,
+    'Food & Drinks': 0.10,
+    'Finance': 0.10,
+    'Public Amenities': 0.10,
+    'Entertainment': 0.10,
+    'Transportation': 0.10,
+    'Accommodation': 0.05,
+    'Nature': 0.05,
+    'Infrastructure': 0.05,
+    'Shopping': 0.05,
+    'Nightlife': 0.05,
+    'Safety': 0.05,
+    'Crime Rate': 0.05
+}
